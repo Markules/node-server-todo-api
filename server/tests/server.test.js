344a8b5e2eb412b5c4c2ffad51b72ed66ed81ba8
Markules <markys.1988@gmail.com>
2020-07-30 09:08:37 +0300

    add tests for GET todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,11 +1,22 @@
 const expect = require("expect");
 const request = require("supertest");
+const {ObjectID} = require('mongodb');
 
 const { app } = require("./../server");
 const { Todos, Todo } = require("./../models/todos");
 
+const todos = [{
+  _id: new ObjectID(),
+  text: 'First test todo'
+}, {
+  _id: new ObjectID(),
+  text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-  Todos.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe("POST /todos", () => {
@@ -14,7 +25,7 @@ describe("POST /todos", () => {
 
     request(app)
       .post("/todos")
-      .send({ text })
+      .send({text})
       .expect(200)
       .expect((res) => {
         expect(res.body.text).toBe(text);
@@ -24,7 +35,7 @@ describe("POST /todos", () => {
           return done(err);
         }
 
-        Todos.find()
+        Todo.find({text})
           .then((todos) => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -38,10 +49,34 @@ describe("POST /todos", () => {
     if (err) {
       return done(err);
     }
-    Todos.find().then((todos) => {
-        expect(todos.length).toBe(0);
+    Todo.find().then((todos) => {
+        expect(todos.length).toBe(2);
         done();
       }).catch((e) => done(e));
   });
 });
 });
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+    .get(`/todos`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
+  })
+});
+
+// describe('GET /todos/id', () => {
+//   it('should return todo doc', (done) => {
+//     request(app)
+//     .get(`/todos/${todos[0]._id.toHexString()}`)
+//     .expect(200)
+//     .expect((res) => {
+//       expect(res.body.todo.text).toBe(todos[0].text);
+//     })
+//     .end(done);
+//   })
+// });
