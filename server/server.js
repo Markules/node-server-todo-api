c22522379a74b3cf41bedf587557b8ce3410b3e2
Markules <markys.1988@gmail.com>
2020-07-30 15:09:17 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require ('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todos');
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id; 
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+}
+  Todo.findById(id).then((todos) => {
+    if(!todos){
+      res.status(404).send(); 
+    }
+
+    res.send({todos});
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
 });
 
 app.listen(3000, () => {
