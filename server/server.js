6942cfd1506a2110a1f91537f49b1669d7af3920
Markules <markys.1988@gmail.com>
2020-08-08 09:52:14 +0300

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-require('./config/config');
+require("./config/config");
 
-
-const _ = require('lodash');
+const _ = require("lodash");
 var express = require("express");
 var bodyParser = require("body-parser");
 const { ObjectID } = require("mongodb");
@@ -9,7 +8,7 @@ const { ObjectID } = require("mongodb");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todos");
 var { User } = require("./models/user");
-var {authenticate} = require("./middleware/authenticate");
+var { authenticate } = require("./middleware/authenticate");
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -66,72 +65,87 @@ app.delete("/todos/:id", (req, res) => {
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-  Todo.findByIdAndRemove(id).then((todo) => {
-    if (todo) {
-      return res.send({todo});
-    }
-    return res.status(404).send();
-  }).catch((e) =>{
+  Todo.findByIdAndRemove(id)
+    .then((todo) => {
+      if (todo) {
+        return res.send({ todo });
+      }
+      return res.status(404).send();
+    })
+    .catch((e) => {
       res.status(400).send();
-  });
+    });
 });
 
-app.patch('/todos/:id', (req, res) => {
+app.patch("/todos/:id", (req, res) => {
   var id = req.params.id;
-  var body = _.pick(req.body, ['text', 'completed']);
+  var body = _.pick(req.body, ["text", "completed"]);
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-   if(_.isBoolean(body.completed) && body.completed) {
-     body.completedAt = new Date().getTime();
-   } else {
-     body.completed = false;
-     body.completedAt = null;
-   }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
 
-   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-     if(!todo) {
-       return res.status(404).send();
-     }
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
 
-     res.send({todo});
-   }).catch((e) => {
-     res.status(400).send();
-   })
+      res.send({ todo });
+    })
+    .catch((e) => {
+      res.status(400).send();
+    });
 });
 
 app.post("/users", (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
+  var body = _.pick(req.body, ["email", "password"]);
   var user = new User(body);
 
-  user.save().then(() => {
-    return user.generateAuthToken();
-  }).then((token) => {
-    res.header('x-auth', token).send(user);
-    }).catch((e) => {
-      res.status(400).send(e);
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
     })
+    .then((token) => {
+      res.header("x-auth", token).send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
 });
 
-
-
-app.get('/users/me', authenticate, (req, res) => {
+app.get("/users/me", authenticate, (req, res) => {
   res.send(req.user);
 });
-  
-app.post('/users/login', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
 
-  User.findByCredentials(body.email, body.password).then((user) => {
-    user.generateAuthToken().then((token) => {
-      res.header('x-auth', token).send(user);
+app.post("/users/login", (req, res) => {
+  var body = _.pick(req.body, ["email", "password"]);
+
+  User.findByCredentials(body.email, body.password)
+    .then((user) => {
+      user.generateAuthToken().then((token) => {
+        res.header("x-auth", token).send(user);
+      });
+    })
+    .catch((e) => {
+      res.status(400).send();
     });
-
-  }).catch((e) => {
-    res.status(400).send();
-  });
 });
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  })
+})
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
